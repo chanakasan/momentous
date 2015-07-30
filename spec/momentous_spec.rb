2bc837d5f568ae90b1a0b9b0613dc0bf95cb153f
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-07-30 13:45:06 +0530

    remove SignupListener, add UserMailer as the listener

--- spec/momentous_spec.rb
+++ spec/momentous_spec.rb
@@ -5,19 +5,8 @@ class UserMailer
   def send_welcome_email(user); end
 end
 
-class SignupListener
-  def initialize(user_mailer = UserMailer.new)
-    @user_mailer = user_mailer
-  end
-
-  def after_signup(user)
-    @user_mailer.send_welcome_email(user)
-  end
-end
-
 RSpec.describe Momentous::EventDispatcher do
   let(:user_mailer) { instance_double(UserMailer) }
-  let(:signup_listener) { SignupListener.new(user_mailer) }
   let(:user) { Struct.new(:name).new }
 
   it 'initially has an empty listeners array' do
@@ -26,29 +15,29 @@ RSpec.describe Momentous::EventDispatcher do
   end
 
   it 'stores listeners for an event' do
-    subject.add_listener(:after_signup, [signup_listener, :after_signup])
+    subject.add_listener(:after_signup, [user_mailer, :send_welcome_email])
 
-    expect(subject.get_listeners(:after_signup)).to eql([[signup_listener, :after_signup]])
+    expect(subject.get_listeners(:after_signup)).to eql([[user_mailer, :send_welcome_email]])
   end
 
   it 'knows whether listeners exist for an event' do
     expect(subject.has_listeners(:after_signup)).to eql(false)
 
-    subject.add_listener(:after_signup, [signup_listener, :after_signup])
+    subject.add_listener(:after_signup, [user_mailer, :send_welcome_email])
     expect(subject.has_listeners(:after_signup)).to eql(true)
   end
 
   it 'removes existing listeners for an event' do
-    subject.add_listener(:after_signup, [signup_listener, :after_signup])
+    subject.add_listener(:after_signup, [user_mailer, :send_welcome_email])
     expect(subject.has_listeners(:after_signup)).to eql(true)
 
-    subject.remove_listener(:after_signup, [signup_listener, :after_signup])
+    subject.remove_listener(:after_signup, [user_mailer, :send_welcome_email])
 
     expect(subject.has_listeners(:after_signup)).to eql(false)
   end
 
   it 'dispatches events' do
-    subject.add_listener(:after_signup, [signup_listener, :after_signup])
+    subject.add_listener(:after_signup, [user_mailer, :send_welcome_email])
 
     expect(user_mailer).to receive(:send_welcome_email).with(user)
     subject.dispatch(:after_signup, user)
